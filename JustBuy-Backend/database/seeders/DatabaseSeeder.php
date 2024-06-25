9c4b7f1f2bdc577129b01ab084ae80f54c9f8ea7
Abubakr <thefox5000@gmail.com>
2024-06-26 02:51:24 +0300

    :gear: Parse json dummy data and seed into database

--- JustBuy-Backend/database/seeders/DatabaseSeeder.php
+++ JustBuy-Backend/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $json = file_get_contents(database_path('products.json'));
+        $data = json_decode($json, true);
+        $products = $data['products'];
 
+
+
+        foreach ($products as $product) {
+            // Categories
+            $category = $product['category'];
+            if (!DB::table('categories')->where('name', $category)->exists()) {
+                $categoryId = DB::table('categories')->insertGetId(['name' => $category]);
+            }
+
+            //product
+            $productId = DB::table('products')->insertGetId([
+                'name' => $product['title'],
+                'description' => $product['description'],
+                'price' => $product['price'],
+                'discount_percentage' => $product['discountPercentage'],
+                'rating' => $product['rating'],
+                'stock' => $product['stock'],
+                'category_id' => $categoryId,
+                'brand' => $product['brand'] ?? null,
+                'sku' => $product['sku'],
+                'weight' => $product['weight'],
+                'shipping_information' => $product['shippingInformation'],
+                'availability_status' => $product['availabilityStatus'],
+                'image_url' => $product['images'][0],
+                'thumbnail_url' => $product['thumbnail'],
+            ]);
+
+            // Subcategories
+            foreach ($product['tags'] as $tag) {
+                if ($tag != $category) {
+                    $subCategory = $tag;
+
+                    if (!DB::table('subcategories')->where('name', $subCategory)->exists()) {
+                        $subCategoryId = DB::table('subcategories')->insertGetId(['name' => $subCategory, 'category_id' => $categoryId]);
+                    }
+
+                    //product_subcategories
+                    DB::table('product_subcategory')->insert(['product_id' => $productId, 'subcategory_id' => $subCategoryId]);
+                }
+            }
+        }
     }
 }
