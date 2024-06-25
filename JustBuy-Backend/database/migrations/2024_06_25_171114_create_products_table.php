7b3767f8b3861f6ecff58b4321fc36c1b924c04d
Abubakr <thefox5000@gmail.com>
2024-06-25 20:15:19 +0300

    :gear: Creat and run database migrations

---
+++ JustBuy-Backend/database/migrations/2024_06_25_171114_create_products_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use App\Models\Category;
+use App\Models\Subcategory;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->foreignIdFor(Category::class);
+            $table->foreignIdFor(Subcategory::class);
+            $table->decimal('price');
+            $table->float('discount_percentage');
+            $table->integer('rating');
+            $table->integer('stock');
+            $table->string('brand');
+            $table->string('sku');
+            $table->integer('weight');
+            $table->string('shipping_information');
+            $table->string('availability_status');
+            $table->string('image_url');
+            $table->string('thumbnail_url');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+
+    }
+};
